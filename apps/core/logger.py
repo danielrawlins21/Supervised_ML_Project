d15d86c938c01c007e76f9c43b0efb7519b1aa54
Daniel Rawlins <danielpr211@hotmail.com>
2024-01-12 22:24:09 -0500

    File operation, config file and logger

---
+++ apps/core/logger.py
@@ -0,0 +1,40 @@
+import logging
+
+
+class Logger:
+    """_summary_
+    **************************************************************************
+    *
+    *filename:       logger.py
+    *version:        1.0
+    *author:         Daniel Rawlins
+    *creation date:  12-Jan-2024
+    *
+    *change history:
+    *
+    *who           when           version    change(include bug# if apply)
+    *----------    -----------    -------    -----------------------------
+    *D. Rawlins    12-JAN-2024       1.0     initial creation
+    *
+    *
+    *description: Class for configuration instance attributes
+    *
+    **************************************************************************
+    """
+    def __init__ (self, run_id, log_module, log_file_name):
+        self.logger = logging.getLogger(str(log_module)+'_'+str(run_id))
+        self.logger.setLevel(logging.DEBUG)
+        if log_file_name == 'training':
+            file_handler = logging.FileHandler('logs/training_logs/train_log_'+str(run_id)+'.log')
+        else:
+            file_handler = logging.FileHandler('logs/prediction_logs/predict_log_'+str(run_id)+'.log')
+        
+        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        
+    def info(self, message):
+        self.logger.info(message)
+    
+    def exception(self, message):
+        self.logger.exception(message)
